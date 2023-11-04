93be9e134738274224a1d070ab9e275e68816327
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2023-11-04 15:07:09 +0000

    basic website working

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def root():
     return render_template('page.html', title='The Infinite Melody')
     
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run(debug=True)
