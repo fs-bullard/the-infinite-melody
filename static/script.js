07e2d136af5c2dcc4f3acb1baf6a346b9051e743
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2023-11-05 00:35:06 +0000

    finally playing sequential notes

--- static/script.js
+++ static/script.js
@@ -1,7 +1,6 @@
 const playButton = document.getElementById("play-button");
 const noteContainer = document.getElementById("note-container");
 
-const audio = new Audio();
 const audioURLs = {
     "A4":"static/piano-mp3/A4.mp3",
     "A#4":"static/piano-mp3/Bb4.mp3",
@@ -18,72 +17,71 @@ const audioURLs = {
 };
 
 // For testing
-const exampleNotes = [["C4", 1500], ["D4", 1500],["E4", 1500],["F4", 1500],["G4", 1500]];
-let noteIndex = 0;
+const exampleNotes = [["C4", 1500], ["D4", 1500], ["E4", 1500], ["F4", 1500], ["G4", 1500]];
+// const exampleNotes = [["C4", 1500]];
 
 // Set animation duration (using 100 bpm)
-// const width = window.screen.availWidth;
-// const crotchetWidth = 100;
-// const bpm = 100;
+const width = window.screen.availWidth;
+const crotchetWidth = 100;
+const bpm = 100;
 
-// const animationDuration = width * (60/bpm) / crotchetWidth * 1000;
-const animationDuration = 1000
+let elapsedTime = 0;
+
+const animationDuration = width * (60/bpm) / crotchetWidth * 1000;
+console.log(animationDuration)
+// const animationDuration = 10000;
 
 function createNoteElement(note) {
-    console.log("Creating note");
-    
     const noteElement = document.createElement("div");
-    // noteElement.className = "note";
 
-    const noteImage = document.createElement("img");
+    let noteImage = document.createElement("img");
+
     noteImage.className = "note";
-    noteImage.src = "static/crochet.png";
+    noteImage.src = 'static/crochet.png';
     noteElement.appendChild(noteImage);
-    noteContainer.appendChild(noteElement);
+    noteContainer.appendChild(noteImage);
+
     return noteElement;
 }
 
-function animateNote(noteElement, animationDuration) {
-    console.log("Animating note");
-    noteElement.style.left = "0%";
-    const animation = noteElement.animate(
-        [{ left: "0" }, { left:"100%" }],
-        { animationDuration, easing: "linear" }
-    );
+function playNextNote(noteIndex) {
+    const audio = new Audio();
 
-    noteIndex++;
-    playNextNote();
+    // Find the current note and its duration
+    const note = exampleNotes[noteIndex][0];
+    const duration = exampleNotes[noteIndex][1];
 
-    animation.onfinish = () => {
-        noteElement.remove();
-    };
+    // Create the note
+    const noteElement = createNoteElement(note);
+
+    audio.src = audioURLs[note];
+    audio.currentTime = 0; // Reset audio to the beginning
+    audio.play();
+
+    elapsedTime += duration;
+    
+    // Schedule the next note's animation
+    console.log(elapsedTime)
+    setTimeout(() => {
+        generateSingleNote(noteIndex + 1);
+    }, duration)
+
+    //TODO: Remove finished notes
+    setTimeout(() => {
+        noteElement.remove;
+    }, elapsedTime + animationDuration)
+
 }
 
-function playNextNote() {
+function generateSingleNote(noteIndex) {
     if (noteIndex < exampleNotes.length) {
-        console.log('Playing note' + noteIndex);
-
-        // Find the current note and its duration
-        const note = exampleNotes[noteIndex][0];
-        const duration = exampleNotes[noteIndex][1];
-
-        // Create the note
-        const noteElement = createNoteElement(note);
-
-        animateNote(noteElement, animationDuration);
-
-        audio.src = audioURLs[note];
-        audio.currentTime = 0; // Reset audio to the beginning
-        audio.play();
-        noteElement.remove();
-        noteIndex++;
-       
-        // Schedule the next note's animation
-        setTimeout(playNextNote(), 1000);
+        playNextNote(noteIndex)
+        noteIndex += 1;
     }
 }
 
 // Start the music playback
 playButton.addEventListener("click", function() {
-    playNextNote();
+    let noteIndex = 0;
+    generateSingleNote(noteIndex);
 });
